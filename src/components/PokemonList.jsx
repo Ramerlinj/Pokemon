675fc1cd1f15d8d323397302a90e9b532b927885
Ramerlin <ramerlint2008@gmail.com>
2025-07-21 16:14:02 -0400

    Improves Pokemon list rendering and loading
    Implements incremental loading of Pokemon and enhances the
    rendering of Pokemon list by displaying them in a grid format.
    It also fixes the search functionality to work with the ID.

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -4,21 +4,23 @@ import usePokemon  from "../hooks/usePokemon"
 
 const PokemonList = () => {
 
-
+    const [visibleCount,setVisibleCount] = useState(9)
     const [search, setSearch] = useState('')
-    const [numsPokemon, setNumsPokemon] = useState(1)
-    const {pokemon, loading, error} = usePokemon(numsPokemon)
+    const {pokemon, loading, error} = usePokemon(1025)
     
     const handleInput = (e) =>{
         setSearch(e.target.value)
     }
     const handleClick = () =>{
-       setNumsPokemon((i) => i+1)
+       setVisibleCount((i) => i+9)
     }
 
-    const filtered = pokemon.filter((p) =>
-        p.name.toLowerCase().includes(search.toLowerCase()) || p.id.includes(search));
+    const filtered = pokemon.filter((p) => 
+        p.name.toLowerCase().includes(search.toLowerCase()) ||
+        p.id.toString().includes(search)
+    );
 
+    const visible = filtered.slice(0, visibleCount);
 
     return (
         <>
@@ -38,17 +40,20 @@ const PokemonList = () => {
             )}
 
            
-            <div className="">
-                {filtered.map((pokemon) => (
+            <div className="grid grid-cols-5">
+                {visible.map((pokemon) => (
                     <>
-                        <p key={pokemon.id}>{pokemon.name}</p>
-                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} />
+                        <div key={pokemon.id} className="">
+
+                            <p>{pokemon.name}</p>
+                            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} />
+                        </div>
                     </>
                         ))}
 
-                   {filtered.length === 0 && !loading && <p>No se encontraron Pokémon</p>}             
-                    <button className="bg-amber-100 text-black w-10" onClick={handleClick}>Ver más</button>
+                   {filtered.length ===0 && ( <p>No se encontraron Pokémon</p>)}    {/*Hacer mejor componente*/}         
                 </div>
+                    <button className="bg-amber-100 text-black w-10" onClick={handleClick}>Ver más</button>
         </>
   );
 };
